d4521c67b4e40135de441a6d8f01dc1cce546b1b
Jordan Hudgens <jordan@devcamp.com>
2017-07-06 14:38:15 +0000

    Impelemented partials with local data for portfolio form and nav bars

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,7 +11,7 @@ class PortfoliosController < ApplicationController
   
   def new
     @portfolio_item = Portfolio.new
-    3.times {@portfolio_item.technologies.build}
+    
   end
 
  def create
@@ -28,6 +28,7 @@ class PortfoliosController < ApplicationController
  
  def edit 
    @portfolio_item = Portfolio.find(params[:id])
+   3.times {@portfolio_item.technologies.build}
  end
   
    def update
