56fcf9c6355cc26c9acb8b1d8a8427b14a972e13
Jordan Hudgens <jordan@devcamp.com>
2017-07-12 14:40:00 +0000

    Integrated the gem into rails app for copyright

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,4 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
-  
-  before_action :set_copyright
-  
-  def set_copyright
-    @copyright = ThisViewTool::Renderer.copyright 'A person', 'All rights reserved'
-  end
-end
-
-module ThisViewTool
-  class Renderer
-    def self.copyright name, msg
-    "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
-    end
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  
+  def copyright_generator
+    ThisViewTool::Renderer.copyright 'A person', 'All rights Reserved' 
+  end
 end
