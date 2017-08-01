9394a377b45a8bb57fb4a8f72ad696238b05c640
Jordan Hudgens <jordan@devcamp.com>
2017-08-01 16:28:02 +0000

    Completed comment feature with actioncable

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,24 @@
 module BlogsHelper
+  def gravatar_helper user
+    image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
+  end
+
+
+  def markdown(text)
+    coderayified = CodeRayify.new(filter_html: true, hard_wrap: true)
+
+    options = {
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      autolink: true,
+      lax_html_blocks: true,
+    }
+
+    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+    markdown_to_html.render(text).html_safe
+  end
+
+  def blog_status_color blog
+    'color: red;' if blog.draft?
+  end
 end
